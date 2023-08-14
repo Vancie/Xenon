95f2ca60423bd73c0b4c80fe33a2c6bda3694d54
Nathan Vance <Natevance01@gmail.com>
2023-08-14 14:45:32 -0400

    Implemented Window events and application now closes on window close event

--- Engine/src/Xenon/Application.hpp
+++ Engine/src/Xenon/Application.hpp
@@ -2,23 +2,26 @@
 #include "XenonPCH.hpp"
 
 #include "Core.hpp"
+#include "Events/ApplicationEvent.hpp"
 #include "Events/Event.hpp"
 #include "Window.hpp"
 
 namespace Xenon {
-  class XENON_API Application {
-    public:
-      Application();
-      virtual ~Application();
-      void Run();
+class XENON_API Application {
+public:
+  Application();
+  virtual ~Application();
+  void Run();
 
-    private:
-      std::unique_ptr<Window> m_Window;
-      bool m_Running = true;
+  void OnEvent(Event &e);
 
-  };
+private:
+  bool OnWindowClose(WindowCloseEvent &e);
+  std::unique_ptr<Window> m_Window;
+  bool m_Running = true;
+};
 
-  // To be defined in client
+// To be defined in client
 
-  Application* CreateApplication();
-}
+Application *CreateApplication();
+} // namespace Xenon
--- Engine/src/Xenon/Events/MouseEvent.hpp
+++ Engine/src/Xenon/Events/MouseEvent.hpp
@@ -1,90 +1,85 @@
 
 
-
 #pragma once
 
 #include "Event.hpp"
 
-
 namespace Xenon {
-  class XENON_API MouseMovedEvent : public Event {
-    public:
-      MouseMovedEvent(float x, float y) : m_MouseX(x), m_MouseY(y) {}
+class XENON_API MouseMovedEvent : public Event {
+public:
+  MouseMovedEvent(float x, float y) : m_MouseX(x), m_MouseY(y) {}
 
-      inline float GetX() const { return m_MouseX; }
-      inline float GetY() const { return m_MouseY; }
+  inline float GetX() const { return m_MouseX; }
+  inline float GetY() const { return m_MouseY; }
 
-      std::string ToString() const override {
-        std::stringstream ss;
-        ss << "MouseMovedEvent: " << m_MouseX << ", " << m_MouseY;
-        return ss.str();
-      }
-      EVENT_CLASS_TYPE(MouseMoved)
-      EVENT_CLASS_CATAGORY(EventCatagoryMouse | EventCatagoryInput)
+  std::string ToString() const override {
+    std::stringstream ss;
+    ss << "MouseMovedEvent: " << m_MouseX << ", " << m_MouseY;
+    return ss.str();
+  }
+  EVENT_CLASS_TYPE(MouseMoved)
+  EVENT_CLASS_CATAGORY(EventCatagoryMouse | EventCatagoryInput)
 
+private:
+  float m_MouseX;
+  float m_MouseY;
+};
 
-    private:
-      float m_MouseX;
-      float m_MouseY;
-  };
+class XENON_API MouseScrolledEvent : public Event {
+public:
+  MouseScrolledEvent(float xOffset, float yOffset)
+      : m_XOffset(xOffset), m_YOffset(yOffset) {}
 
+  inline float GetXOffset() const { return m_XOffset; }
+  inline float GetYOffset() const { return m_YOffset; }
 
-  class XENON_API MouseScrolledEvent : public Event {
-    public:
-      MouseScrolledEvent(float xOffset, float yOffset) : m_XOffset(xOffset), m_YOffset(yOffset) {}
+  std::string ToString() const override {
+    std::stringstream ss;
+    ss << "MouseScrolledEvent: " << m_XOffset << ", " << m_YOffset;
+    return ss.str();
+  }
 
-      inline float GetXOffset() const { return m_XOffset; }
-      inline float GetYOffset() const { return m_YOffset; }
+  EVENT_CLASS_TYPE(MouseScrolled)
+  EVENT_CLASS_CATAGORY(EventCatagoryMouse | EventCatagoryInput)
+private:
+  float m_XOffset;
+  float m_YOffset;
+};
 
-      std::string ToString() const override {
-        std::stringstream ss;
-        ss << "MouseScrolledEvent: " << m_XOffset << ", " << m_YOffset;
-        return ss.str();
-      }
+class XENON_API MouseButtonEvent : public Event {
+public:
+  inline int GetMouseButton() const { return m_Button; }
 
-      EVENT_CLASS_TYPE(MouseScrolled)
-      EVENT_CLASS_CATAGORY(EventCatagoryMouse | EventCatagoryInput)
-    private:
-      float m_XOffset;
-      float m_YOffset;
-  };
+  EVENT_CLASS_CATAGORY(EventCatagoryMouse | EventCatagoryInput)
+protected:
+  MouseButtonEvent(int button) : m_Button(button) {}
 
-  class XENON_API MouseButtonEvent : public Event {
-    public:
-      inline int GetMouseButton() const { return m_Button; }
-      
-      EVENT_CLASS_CATAGORY(EventCatagoryMouse | EventCatagoryInput)
-    protected:
-        MouseButtonEvent(int button) : m_Button(button) {}
+  int m_Button;
+};
 
-        int m_Button;
-  };
+class XENON_API MouseButtonPressedEvent : public MouseButtonEvent {
+public:
+  MouseButtonPressedEvent(int button) : MouseButtonEvent(button) {}
+  std::string ToString() const override {
+    std::stringstream ss;
+    ss << "MouseButtonPressedEvent: " << m_Button;
+    return ss.str();
+  }
 
-  class XENON_API MouseButtonPressedEvent : public MouseButtonEvent {
-    public:
-      MouseButtonPressedEvent(int button) : MouseButtonEvent(button) {}
-      std::string ToString() const override {
-        std::stringstream ss;
-        ss << "MouseButtonPressedEvent: " << m_Button;
-        return ss.str();
-      }
+  EVENT_CLASS_TYPE(MouseButtonPressed)
+};
 
-      EVENT_CLASS_TYPE(MouseButtonPressed)
-  };
+class XENON_API MouseButtonReleasedEvent : public MouseButtonEvent {
+public:
+  MouseButtonReleasedEvent(int button) : MouseButtonEvent(button) {}
 
-  class XENON_API MouseBUttonReleasedEvent : public MouseButtonEvent {
-    public:
+  std::string ToString() const override {
 
-      MouseBUttonReleasedEvent(int button) : MouseButtonEvent(button) {}
+    std::stringstream ss;
+    ss << "MouseButtonReleasedEvent: " << m_Button;
+    return ss.str();
+  }
 
-      std::string ToString() const override {
-
-        std::stringstream ss;
-        ss << "MouseButtonReleasedEvent: " << m_Button;
-        return ss.str();
-
-      }
-
-      EVENT_CLASS_TYPE(MouseButtonReleased)
-  };
-}
+  EVENT_CLASS_TYPE(MouseButtonReleased)
+};
+} // namespace Xenon
